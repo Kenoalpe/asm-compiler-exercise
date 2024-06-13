883d5e5f48d9bbde1f976585ae5b50b756ec6cc5
kenoalpe <git@kenoalbers.de>
2024-06-13 20:55:19 +0200

    feat: add file_parse

--- app/main.py
+++ app/main.py
@@ -1,14 +1,19 @@
+# External imports
 from sly import Lexer, Parser
 
-global INSTRUCTION, NOP
+# Internal imports
+from app.util import FileUtils
 
 
-class CalcLexer(Lexer):
+class AsmLexer(Lexer):
     # Available Token
     tokens = {INSTRUCTION, NOP}
 
     # Ignore whitespace and tab
     ignore = " \t"
+
+    # Ignore assembler
+    ignore_comment = r'\;.*'
 
     # Tokens
     INSTRUCTION = r'[A-Z][A-Z]*'
@@ -25,14 +30,44 @@ class CalcLexer(Lexer):
         self.index += 1
 
 
+class AsmParser(Parser):
+    tokens = AsmLexer.tokens
+
+    def __init__(self):
+        self.env = {}
+
+    @_('INSTRUCTION')
+    def statement(self, p):
+        print('Test')
+
+    # Grammar rules and actions
+    @_('NOP')
+    def statement(self, p):
+        print('NOP -> PRINT')
+
+
 def run():
-    data = '''
-NOP
-NOP
-'''
+    print('Welcome, let\'s assemble some assembly!\n')
 
-    lexer = CalcLexer()
-    for token in lexer.tokenize(data):
-        print('type=%r, value=%r' % (token.type, token.value))
+    # Assembly program code parse
+    data = FileUtils.parse_txt('assembler/asm-1.txt')
 
-    print(lexer)
+    print('Assembly code to assemble:')
+    print('__________________________\n')
+    if data is not None:
+        print(data)
+
+
+
+    # Get instances of lexer and parser
+    #lexer = AsmLexer()
+    #parser = AsmParser()
+
+    # Split file into tokens
+    #tokenized = lexer.tokenize(data)
+
+    # Parse tokens to machine code
+    #parser.parse(tokenized)
+
+    #for token in tokenized:
+    #    print('line=%r, type=%r, value=%r' % (token.lineno, token.type, token.value))
