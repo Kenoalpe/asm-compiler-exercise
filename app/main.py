6f4075de5d6c9a81aa06a9a753bd50f49a5f774f
Jannik <jannik.hennicke@gmail.com>
2024-06-13 13:06:31 +0200

    feat: add Lexer, Parser and some tokens

--- app/main.py
+++ app/main.py
@@ -1,4 +1,37 @@
 from sly import Lexer, Parser
 
+machinecode = {
+    "NOP" : "20"
+}
+
+class CalcLexer(Lexer):
+    tokens = {INSTRUCTION, ADDRESS, HEX_NUMBERS, SPECIAL_CHARS, REGISTERS, COMMENTS, NOP}
+    ignore = ' \t'
+    ignore_comment = r'\;.*'
+
+    # Tokens
+    INSTRUCTION = r'[A-Z]*'
+    ADDRESS = r'[a-zA-Z_][a-zA-Z0-9]*\:'
+    HEX_NUMBERS = r'\#[0-9]*'
+    #SPECIAL_CHARS = r':'
+    REGISTERS = r'a|b'
+
+    INSTRUCTION['NOP'] = NOP
+
+     # Line number tracking
+    @_(r'\n+')
+    def ignore_newline(self, t):
+        self.lineno += t.value.count('\n')
+    
+    # Error handling rule
+    def error(self, t):
+        print("Illegal character '%s'" % t.value[0])
+        self.index += 1
+    
+class CalcParser(Parser):
+    tokens = CalcLexer.tokens
+
+
 def run():
-    print("Hello COMARCH")
+    lexer = CalcLexer().tok
+    parser = CalcParser()
