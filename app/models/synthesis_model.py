62644ac097e4c86d4a12cd8c515921130a2fdb0e
kenoalpe <git@kenoalbers.de>
2024-06-15 11:49:52 +0200

    feat: synthesis model

--- app/models/synthesis_model.py
+++ app/models/synthesis_model.py
@@ -1,2 +1,13 @@
 class SynthesisModel:
-    pass
+    def __init__(self, symbol_table, literal_table, opcode_table=None):
+        self.symbol_table = symbol_table
+        self.literal_table = literal_table
+        self._opcode_table = opcode_table
+
+    @property
+    def opcode_table(self):
+        return self._opcode_table
+
+    @opcode_table.setter
+    def opcode_table(self, opcode_table):
+        self._opcode_table = opcode_table
