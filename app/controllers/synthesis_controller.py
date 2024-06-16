09e8b1a1f6ecdd45d2b7c3c3ca0d6d611bb0a221
kenoalpe <git@kenoalbers.de>
2024-06-16 14:22:21 +0200

    feat: changes

--- app/controllers/synthesis_controller.py
+++ app/controllers/synthesis_controller.py
@@ -15,6 +15,7 @@ class SynthesisController:
         # Initialize variables
         found_instruction = False
         opcode_list_out = []
+        opcode_add_counter = 0
 
         # ToDo remove debug
         print('---------------------')
@@ -28,6 +29,11 @@ class SynthesisController:
             if not line:
                 continue
 
+            # Remove label
+            line = AssemblerUtil.remove_label(line=line)
+            if not line:
+                continue
+
             print(line)
             # Replace instruction with opcode
             for key in self.model.opcode_table:
@@ -36,17 +42,30 @@ class SynthesisController:
                 if match:
                     opcode_list_out.append(self.model.opcode_table[key])
                     found_instruction = True
-                    print('true')
                     break
 
             # Check if any instruction was found
             if not found_instruction:
                 raise SyntaxError(f"Instruction on line '{line}' not available in opcode table!")
-            print()
 
-            # for key in self.model.opcode_table:
-            #     match = re.match(r'^\w+$', key)
-            #     if match:
-            #         opcode_list_out.append(self.model.opcode_table[key])
+            # Check if a label need to be placed
+            for key in self.model.symbol_table:
+                match = re.search(pattern=key, string=line)
+                if match:
+                    opcode_list_out.append(opcode_list_out[self.model.symbol_table[key] + opcode_add_counter])
+                    break
+
+            # Check if a constant needs to be added
+            const_value_pattern = r'#([0-9a-fA-F]{1,2})'
+            match = re.search(pattern=const_value_pattern, string=line)
+            if match:
+                const_value = match.group(1)
+                # Add a 0 when too short
+                if len(const_value) == 1:
+                    const_value = f'0{const_value}'
+                opcode_list_out.append(const_value)
+                opcode_add_counter += 1
+
+            print()
 
         print(opcode_list_out)
--- app/util/assembler_util.py
+++ app/util/assembler_util.py
@@ -1,3 +1,6 @@
+import re
+
+
 class AssemblerUtil:
     def __init__(self):
         raise NotImplementedError('This class is not meant to be instantiated')
@@ -6,3 +9,11 @@ class AssemblerUtil:
     def remove_comments_line(line: str):
         # Prepare line and remove comments
         return line.split(';', 1)[0].strip()
+
+    @staticmethod
+    def remove_label(line: str):
+        label_pattern = r'([_a-z]\w*)\s*:'
+        match = re.match(pattern=label_pattern, string=line)
+        if match:
+            line = line.replace(match.group(1) + ':', '', 1).strip()  # Remove label from line
+        return line
